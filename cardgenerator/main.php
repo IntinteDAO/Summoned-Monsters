cbfa142fc85a7fdd3a35821153c8ecbc1ee48eee
fervi <lordfervi@protonmail.com>
2019-11-04 19:39:09 +0100

    Added a card border

--- cardgenerator/main.php
+++ cardgenerator/main.php
@@ -5,8 +5,22 @@ function generate_card($text, $description, $id, $alias, $type, $level, $atk, $d
 // Initialize variables
 include("config.php");
 $image = new Imagick();
+
+// Background
 $image->newPseudoImage($resolution_x, $resolution_y, 'gradient:#000000-#ffffff');
 
+// Border
+$border = new ImagickDraw();
+$border->setFillColor( 'none' );
+
+for($i=0; $i<=$scale; $i++) {
+$color = $i*20;
+$border->setStrokeColor( new ImagickPixel('rgb('.$color.', '.$color.', '.$color.')') );
+$border->setStrokeWidth('1');
+$border->setStrokeAntialias( false );
+$border->rectangle($i, $i, $resolution_x - 1 - $i, $resolution_y - 1 - $i);
+$image->drawImage( $border );
+}
 
 
 
