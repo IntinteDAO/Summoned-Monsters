4300345a08690de0d7d87f2278f1e88845253445
fervi <thunderstormpolska@o2.pl>
2019-11-04 19:19:38 +0100

    Simple gradient generator

---
+++ cardgenerator/config.php
@@ -0,0 +1,11 @@
+<?php
+
+// Variables
+$scale=1;
+
+
+// Don't touch!
+$resolution_x = 177*$scale;
+$resolution_y = 254*$scale;
+
+?>
--- cardgenerator/main.php
+++ cardgenerator/main.php
@@ -2,7 +2,18 @@
 
 function generate_card($text, $description, $id, $alias, $type, $level, $atk, $def, $race, $attribute, $limit) {
 
+// Initialize variables
+include("config.php");
+$image = new Imagick();
+$image->newPseudoImage($resolution_x, $resolution_y, 'gradient:#000000-#ffffff');
+
+
+
+
+$image->setImageFormat('png');
+file_put_contents ("output.png", $image);
 }
 
+echo generate_card("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k");
 
 ?>
