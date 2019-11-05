46b40145238fba7e773ce245710c5717dcd31364
fervi <lordfervi@protonmail.com>
2019-11-05 12:19:28 +0100

    Added font configuration

--- cardgenerator/config.php
+++ cardgenerator/config.php
@@ -2,10 +2,12 @@
 
 // Variables
 $scale=3;
+$font_size = 11;
 
 // Don't touch!
 $resolution_x = 177*$scale;
 $resolution_y = 254*$scale;
 $X1_rail = $scale*2;
 $X2_rail = $resolution_x - ($scale*2);
+$font_size = $font_size * $scale;
 ?>
