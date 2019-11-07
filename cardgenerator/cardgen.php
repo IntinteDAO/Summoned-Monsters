2861c22008d017cc48bb9ade21998563d4bfcb9f
fervi <lordfervi@protonmail.com>
2019-11-07 15:49:31 +0100

    Added different backgrounds for cards

--- cardgenerator/cardgen.php
+++ cardgenerator/cardgen.php
@@ -7,7 +7,15 @@ include("config.php");
 $image = new Imagick();
 
 // Background
-$image->newPseudoImage($resolution_x, $resolution_y, 'gradient:#000000-#ffffff');
+if($type==17) { $color1 = 'rgb(176, 176, 83)'; $color2 = 'rgb(165, 132, 51)'; } // Normal
+else if($type==33 || $type==545 || $type==1057 || $type==2081 || $type==2097185 || $type==4194337) { $color1 = 'rgb(184, 133, 93)'; $color2 = 'rgb(168, 85, 51)'; } // Effect
+else if($type==2 || $type==130 || $type==65538 || $type==131074 || $type==262146 || $type==524290) { $color1 = 'rgb(93, 161, 140)'; $color2 = 'rgb(64, 85, 70)'; } // Magic
+else if($type==4 || $type==131076 || $type==1048580) { $color1 = 'rgb(205, 140, 170)'; $color2 = 'rgb(124, 66, 107)'; } // Trap
+else if($type==65 || $type==97) { $color1 = 'rgb(123, 76, 154)'; $color2 = 'rgb(103, 80, 122)'; } // Fusion
+else if($type==129 || $type==161 || $type==673) { $color1 = 'rgb(107, 117, 170)'; $color2 = 'rgb(65, 74, 91)'; } // Ritual
+else { die("Unknown type"); }
+
+$image->newPseudoImage($resolution_x, $resolution_y, 'gradient:'.$color1.'-'.$color2.'');
 
 // Border
 $border = new ImagickDraw();
@@ -54,6 +62,6 @@ $image->setImageFormat('png');
 file_put_contents ("output.png", $image);
 }
 
-echo generate_card("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k");
+echo generate_card("a", "b", "c", "d", 17, "f", "g", "h", "i", "j", "k");
 
 ?>
