9392ef2c55f2568e676f84c007fb34631b5b1d17
Fervi <lordfervi@protonmail.com>
2021-01-13 15:48:29 +0100

    Added Armageddon card

---
+++ cardgenerator/cards/scripts/Armageddon.lua
@@ -0,0 +1,19 @@
+function c1006.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(c1006.target)
+	e1:SetOperation(c1006.activate)
+	c:RegisterEffect(e1)
+end
+function c1006.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(aux.TRUE,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	local sg=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,sg,sg:GetCount(),0,0)
+end
+function c1006.activate(e,tp,eg,ep,ev,re,r,rp)
+	local sg=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
+	Duel.Destroy(sg,REASON_EFFECT)
+end
