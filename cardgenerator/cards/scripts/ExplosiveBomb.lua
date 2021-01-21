2a0e7f3153dcf16b64e0e8113ae0176928e72c68
Fervi <lordfervi@protonmail.com>
2021-01-21 14:57:42 +0100

    Added "Explosive Bomb" card

---
+++ cardgenerator/cards/scripts/ExplosiveBomb.lua
@@ -0,0 +1,41 @@
+function c1020.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_DAMAGE)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_END_PHASE)
+	e1:SetCountLimit(1,83555666+EFFECT_COUNT_CODE_OATH)
+	e1:SetCondition(c1020.condition)
+	e1:SetTarget(c1020.target)
+	e1:SetOperation(c1020.activate)
+	c:RegisterEffect(e1)
+end
+function c1020.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()~=tp
+end
+function c1020.filter(c,lp)
+	return c:IsFaceup() and c:IsAttackBelow(lp)
+end
+function c1020.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local lp=Duel.GetLP(1-tp)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and c1020.filter(chkc,lp) end
+	if chk==0 then return Duel.IsExistingTarget(c1020.filter,tp,0,LOCATION_MZONE,1,nil,lp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,c1020.filter,tp,0,LOCATION_MZONE,1,1,nil,lp)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,PLAYER_ALL,0)
+end
+function c1020.activate(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and tc:IsFaceup() and Duel.Destroy(tc,REASON_EFFECT)~=0 then
+		local atk=tc:GetTextAttack()
+		if atk<0 then atk=0 end
+		local val=Duel.Damage(tp,atk,REASON_EFFECT)
+		if val>0 and Duel.GetLP(tp)>0 then
+			Duel.BreakEffect()
+			Duel.Damage(1-tp,val,REASON_EFFECT)
+		end
+	end
+end
