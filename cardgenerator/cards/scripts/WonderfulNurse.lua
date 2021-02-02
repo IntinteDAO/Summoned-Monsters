a67e1bcbd346a0d3120295c5ea3dc3a506bd454b
Fervi <lordfervi@protonmail.com>
2021-02-02 16:41:17 +0100

    Added Wonderful Nurse card

---
+++ cardgenerator/cards/scripts/WonderfulNurse.lua
@@ -0,0 +1,31 @@
+function c1026.initial_effect(c)
+	--recover
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(85802526,0))
+	e1:SetCategory(CATEGORY_RECOVER)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCondition(c1026.condition)
+	e1:SetTarget(c1026.target)
+	e1:SetOperation(c1026.operation)
+	c:RegisterEffect(e1)
+end
+function c1026.condition(e,tp,eg,ep,ev,re,r,rp)
+	return tp==Duel.GetTurnPlayer()
+end
+function c1026.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(500)
+	Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,500)
+end
+function c1026.operation(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and c:IsFaceup() then
+		Duel.Recover(p,d,REASON_EFFECT)
+	end
+end
