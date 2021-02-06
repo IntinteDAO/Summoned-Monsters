81dbef98a18c5493e2b1bfe8364a107588011b47
Fervi <lordfervi@protonmail.com>
2021-02-06 20:08:07 +0100

    Added "Earthquake" card

---
+++ cardgenerator/cards/scripts/Earthquake.lua
@@ -0,0 +1,32 @@
+function c1032.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetProperty(EFFECT_FLAG_CONTINUOUS_TARGET)
+	e1:SetTarget(c1032.target)
+	e1:SetOperation(c1032.operation)
+	c:RegisterEffect(e1)
+end
+function c1032.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE,PLAYER_NONE,0)
+		+Duel.GetLocationCount(1-tp,LOCATION_MZONE,PLAYER_NONE,0)>1 end
+	local dis=Duel.SelectDisableField(tp,2,LOCATION_MZONE,LOCATION_MZONE,0xe000e0)
+	e:SetLabel(dis)
+	Duel.Hint(HINT_ZONE,tp,dis)
+end
+function c1032.operation(e,tp,eg,ep,ev,re,r,rp)
+	local zone=e:GetLabel()
+	if tp==1 then
+		zone=((zone&0xffff)<<16)|((zone>>16)&0xffff)
+	end
+	local c=e:GetHandler()
+	--disable field
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCode(EFFECT_DISABLE_FIELD)
+	e2:SetValue(zone)
+	e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+	c:RegisterEffect(e2)
+end
