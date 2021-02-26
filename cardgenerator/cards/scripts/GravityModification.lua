88ea9547bac122bc1bb18ca4b1568624d8804e46
Fervi <lordfervi@protonmail.com>
2021-02-26 20:56:42 +0100

    Added "Gravity Modification"

---
+++ cardgenerator/cards/scripts/GravityModification.lua
@@ -0,0 +1,18 @@
+function c1052.initial_effect(c)
+	--activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--cannot attack
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_CANNOT_ATTACK)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e2:SetTarget(c1052.atktarget)
+	c:RegisterEffect(e2)
+end
+function c1052.atktarget(e,c)
+	return c:IsLevelAbove(4)
+end
