9d27afbd847f3e2713cadb0fd9e421d6e77e1452
Fervi <lordfervi@protonmail.com>
2021-03-03 21:28:23 +0100

    Added "Cyber Lancer" card

---
+++ cardgenerator/cards/scripts/CyberLancer.lua
@@ -0,0 +1,23 @@
+function c1058.initial_effect(c)
+	--to defense
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_DAMAGE_STEP_END)
+	e1:SetCondition(c1058.poscon)
+	e1:SetOperation(c1058.posop)
+	c:RegisterEffect(e1)
+	--pierce
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_PIERCE)
+	c:RegisterEffect(e2)
+end
+function c1058.poscon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler()==Duel.GetAttacker() and e:GetHandler():IsRelateToBattle()
+end
+function c1058.posop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsAttackPos() then
+		Duel.ChangePosition(c,POS_FACEUP_DEFENSE)
+	end
+end
