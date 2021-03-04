6a42fffe320254f2e0ac798d2ce306e971461c28
Fervi <lordfervi@protonmail.com>
2021-03-04 12:31:25 +0100

    Added "Spy" card

---
+++ cardgenerator/cards/scripts/Spy.lua
@@ -0,0 +1,8 @@
+function c1059.initial_effect(c)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_PUBLIC)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTargetRange(LOCATION_HAND,LOCATION_HAND)
+	c:RegisterEffect(e1)
+end
