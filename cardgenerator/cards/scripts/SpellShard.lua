ba1b08d2fa63a1d3b34711a433d47b02ed478a4e
Fervi <lordfervi@protonmail.com>
2021-03-18 17:35:46 +0100

    Added "Spell Shard" card

---
+++ cardgenerator/cards/scripts/SpellShard.lua
@@ -0,0 +1,34 @@
+function c1065.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--cannot activate
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetCode(EFFECT_CANNOT_ACTIVATE)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetTargetRange(1,1)
+	e2:SetValue(c1065.aclimit)
+	c:RegisterEffect(e2)
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e3:SetCode(EVENT_SSET)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetOperation(c1065.aclimset)
+	c:RegisterEffect(e3)
+end
+function c1065.aclimit(e,re,tp)
+	if not re:IsHasType(EFFECT_TYPE_ACTIVATE) or not re:IsActiveType(TYPE_SPELL) then return false end
+	local c=re:GetHandler()
+	return not c:IsLocation(LOCATION_SZONE) or c:GetFlagEffect(1065)>0
+end
+function c1065.aclimset(e,tp,eg,ep,ev,re,r,rp)
+	local tc=eg:GetFirst()
+	while tc do
+		tc:RegisterFlagEffect(1065,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END+RESET_OPPO_TURN,0,1)
+		tc=eg:GetNext()
+	end
+end
