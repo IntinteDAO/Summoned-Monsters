8cd3e25b8963ce11519e4dfa4208115a36bbe238
Fervi <lordfervi@protonmail.com>
2021-04-15 20:52:11 +0200

    Added "Bad Bee" card

---
+++ cardgenerator/cards/scripts/BadBee.lua
@@ -0,0 +1,20 @@
+function c1087.initial_effect(c)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetTarget(c1087.tg1)
+	e1:SetValue(500)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetTarget(c1087.tg2)
+	e2:SetValue(-400)
+	c:RegisterEffect(e2)
+end
+function c1087.tg1(e,c)
+	return c:IsAttribute(ATTRIBUTE_WIND)
+end
+function c1087.tg2(e,c)
+	return c:IsAttribute(ATTRIBUTE_EARTH)
+end
