48fc2babf59e456424abd7b3b803ebb330c9bcae
Fervi <lordfervi@protonmail.com>
2021-04-17 21:04:15 +0200

    Added "Shadow Core" card

---
+++ cardgenerator/cards/scripts/ShadowCore.lua
@@ -0,0 +1,11 @@
+function c1089.initial_effect(c)
+	--fusion substitute
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_FUSION_SUBSTITUTE)
+	e1:SetCondition(c1089.subcon)
+	c:RegisterEffect(e1)
+end
+function c1089.subcon(e)
+	return e:GetHandler():IsLocation(LOCATION_HAND+LOCATION_ONFIELD+LOCATION_GRAVE)
+end
