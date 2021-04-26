15f64a66d7b8c7a0e94b49c063e6d4a501d9e015
Fervi <lordfervi@protonmail.com>
2021-04-26 18:49:36 +0200

    Added "Dark Ritual" card

---
+++ cardgenerator/cards/scripts/DarkRitual.lua
@@ -0,0 +1,93 @@
+function c1097.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCost(c1097.cost1)
+	e1:SetTarget(c1097.target1)
+	e1:SetOperation(c1097.activate1)
+	c:RegisterEffect(e1)
+	--instant
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(1097,0))
+	e3:SetCategory(CATEGORY_SUMMON)
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetCode(EVENT_FREE_CHAIN)
+	e3:SetCondition(c1097.condition2)
+	e3:SetCost(c1097.cost2)
+	e3:SetTarget(c1097.target2)
+	e3:SetOperation(c1097.activate2)
+	c:RegisterEffect(e3)
+end
+function c1097.filter(c)
+	return c:IsSummonable(true,nil) or c:IsMSetable(true,nil)
+end
+function c1097.cost1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	e:SetLabel(0)
+	local tn=Duel.GetTurnPlayer()
+	local ph=Duel.GetCurrentPhase()
+	if ((tn==tp and (ph==PHASE_MAIN1 or ph==PHASE_MAIN2)) or (tn~=tp and ph>=PHASE_BATTLE_START and ph<=PHASE_BATTLE))
+			and Duel.CheckLPCost(tp,850)
+			and Duel.IsExistingMatchingCard(c1097.filter,tp,LOCATION_HAND+LOCATION_MZONE,0,1,nil)
+			and Duel.SelectYesNo(tp,aux.Stringid(1097,1)) then
+		Duel.PayLPCost(tp,850)
+		e:SetLabel(1)
+	end
+end
+function c1097.target1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	if e:GetLabel()~=1 then return end
+	Duel.SetOperationInfo(0,CATEGORY_SUMMON,nil,1,0,0)
+end
+function c1097.activate1(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	if e:GetLabel()~=1 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SUMMON)
+	local g=Duel.SelectMatchingCard(tp,c1097.filter,tp,LOCATION_HAND+LOCATION_MZONE,0,1,1,nil)
+	local tc=g:GetFirst()
+	if tc then
+		if tc:IsSummonable(true,nil) and (not tc:IsMSetable(true,nil)
+			or Duel.SelectPosition(tp,tc,POS_FACEUP_ATTACK+POS_FACEDOWN_DEFENSE)==POS_FACEUP_ATTACK) then
+			Duel.Summon(tp,tc,true,nil)
+		else Duel.MSet(tp,tc,true,nil) end
+	end
+end
+function c1097.condition2(e,tp,eg,ep,ev,re,r,rp)
+	local tn=Duel.GetTurnPlayer()
+	local ph=Duel.GetCurrentPhase()
+	return (tn==tp and (ph==PHASE_MAIN1 or ph==PHASE_MAIN2)) or (tn~=tp and ph>=PHASE_BATTLE_START and ph<=PHASE_BATTLE)
+end
+function c1097.cost2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckLPCost(tp,850)
+	else Duel.PayLPCost(tp,850)	end
+end
+function c1097.target2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		if not e:GetHandler():IsStatus(STATUS_CHAINING) then
+			local ct=Duel.GetMatchingGroupCount(c1097.filter,tp,LOCATION_HAND+LOCATION_MZONE,0,nil)
+			e:SetLabel(ct)
+			return ct>0
+		else return e:GetLabel()>0 end
+	end
+	e:SetLabel(e:GetLabel()-1)
+	Duel.SetOperationInfo(0,CATEGORY_SUMMON,nil,1,0,0)
+end
+function c1097.activate2(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SUMMON)
+	local g=Duel.SelectMatchingCard(tp,c1097.filter,tp,LOCATION_HAND+LOCATION_MZONE,0,1,1,nil)
+	local tc=g:GetFirst()
+	if tc then
+		local s1=tc:IsSummonable(true,nil)
+		local s2=tc:IsMSetable(true,nil)
+		if (s1 and s2 and Duel.SelectPosition(tp,tc,POS_FACEUP_ATTACK+POS_FACEDOWN_DEFENSE)==POS_FACEUP_ATTACK) or not s2 then
+			Duel.Summon(tp,tc,true,nil)
+		else
+			Duel.MSet(tp,tc,true,nil)
+		end
+	end
+end
