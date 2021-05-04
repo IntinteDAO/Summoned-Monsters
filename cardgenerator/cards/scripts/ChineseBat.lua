d88c58b21e31b34775b2fde0532b76a28dd412e0
Fervi <lordfervi@protonmail.com>
2021-05-04 22:19:53 +0200

    Added "Chinese Bat" card

---
+++ cardgenerator/cards/scripts/ChineseBat.lua
@@ -0,0 +1,43 @@
+function c1103.initial_effect(c)
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(1103,0))
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e1:SetCode(EVENT_BATTLED)
+	e1:SetCondition(c1103.condition)
+	e1:SetOperation(c1103.operation)
+	c:RegisterEffect(e1)
+end
+function c1103.condition(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler()==Duel.GetAttackTarget() and not Duel.GetAttacker():IsRace(RACE_MACHINE)
+end
+function c1103.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetAttacker()
+	if tc:IsRelateToBattle() then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetCode(EVENT_PHASE+PHASE_END)
+		e1:SetCountLimit(1)
+		e1:SetRange(LOCATION_MZONE)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		e1:SetCondition(c1103.descon)
+		e1:SetOperation(c1103.desop)
+		e1:SetLabel(0)
+		e1:SetOwnerPlayer(tp)
+		tc:RegisterEffect(e1)
+	end
+end
+function c1103.descon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetOwnerPlayer()~=Duel.GetTurnPlayer()
+end
+function c1103.desop(e,tp,eg,ep,ev,re,r,rp)
+	local ct=e:GetLabel()
+	ct=ct+1
+	e:SetLabel(ct)
+	e:GetOwner():SetTurnCounter(ct)
+	if ct==3 then
+		Duel.Destroy(e:GetHandler(),REASON_EFFECT)
+	end
+end
