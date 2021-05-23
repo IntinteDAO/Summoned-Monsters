7cf5378476202a68f869df3fac2ed2ed8190fee0
Fervi <lordfervi@protonmail.com>
2021-05-23 23:23:31 +0200

    Added "Bribe" card

---
+++ cardgenerator/cards/scripts/Bribe.lua
@@ -0,0 +1,32 @@
+function c1119.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--attack cost
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_ATTACK_COST)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetTargetRange(1,1)
+	e2:SetCost(c1119.atcost)
+	e2:SetOperation(c1119.atop)
+	c:RegisterEffect(e2)
+	--accumulate
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(0x10000000+1119)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e3:SetTargetRange(1,1)
+	c:RegisterEffect(e3)
+end
+function c1119.atcost(e,c,tp)
+	local ct=Duel.GetFlagEffect(tp,1119)
+	return Duel.CheckLPCost(tp,ct*500)
+end
+function c1119.atop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.PayLPCost(tp,500)
+end
