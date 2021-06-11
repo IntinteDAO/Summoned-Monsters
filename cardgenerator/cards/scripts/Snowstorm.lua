aec886f8c2303683c0ac6f3f94f786d68dddb561
Fervi <lordfervi@protonmail.com>
2021-06-11 22:54:22 +0200

    Added "Snowstorm" card

---
+++ cardgenerator/cards/scripts/Snowstorm.lua
@@ -0,0 +1,34 @@
+function c1134.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(0,TIMING_END_PHASE)
+	e1:SetTarget(c1134.target)
+	e1:SetOperation(c1134.activate)
+	c:RegisterEffect(e1)
+end
+function c1134.filter(c)
+	return c:IsType(TYPE_SPELL+TYPE_TRAP)
+end
+function c1134.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() and chkc:IsControler(1-tp) and c1134.filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c1134.filter,tp,0,LOCATION_ONFIELD,1,e:GetHandler()) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,c1134.filter,tp,0,LOCATION_ONFIELD,1,1,e:GetHandler())
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+end
+function c1134.activate(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) and Duel.Destroy(tc,REASON_EFFECT)~=0 then
+		local g=Duel.GetMatchingGroup(Card.IsSSetable,tp,LOCATION_HAND,0,nil)
+		if g:GetCount()>0 and Duel.SelectYesNo(tp,aux.Stringid(1134,0)) then
+			Duel.BreakEffect()
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+			local sg=g:Select(tp,1,1,nil)
+			Duel.SSet(tp,sg,tp,false)
+		end
+	end
+end
