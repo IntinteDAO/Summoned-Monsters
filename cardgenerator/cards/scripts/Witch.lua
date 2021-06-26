98321a8fb478e0df252d834e7b7597bfb9b037a9
Fervi <lordfervi@protonmail.com>
2021-06-26 19:25:25 +0200

    Added "Witch" card

---
+++ cardgenerator/cards/scripts/Witch.lua
@@ -0,0 +1,33 @@
+function c1147.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(1147,0))
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_FLIP)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetTarget(c1147.target)
+	e1:SetOperation(c1147.operation)
+	c:RegisterEffect(e1)
+end
+function c1147.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and chkc:IsFaceup() end
+	if chk==0 then return true end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	Duel.SelectTarget(tp,nil,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,e:GetHandler())
+end
+function c1147.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if c:IsRelateToEffect(e) and tc and tc:IsRelateToEffect(e)
+		and not tc:IsImmuneToEffect(e) then
+		c:SetCardTarget(tc)
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_CANNOT_ATTACK)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		e1:SetCondition(c1147.rcon)
+		tc:RegisterEffect(e1)
+	end
+end
+function c1147.rcon(e)
+	return e:GetOwner():IsHasCardTarget(e:GetHandler())
+end
