7b7fafc51f8fd20213252eb7f91b73812e745e63
Fervi <lordfervi@protonmail.com>
2021-07-08 20:15:14 +0200

    Added "Flaming Dart" card

---
+++ cardgenerator/cards/scripts/FlamingDart.lua
@@ -0,0 +1,21 @@
+function c1159.initial_effect(c)
+	--damage
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCategory(CATEGORY_DAMAGE)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(c1159.damtg)
+	e1:SetOperation(c1159.damop)
+	c:RegisterEffect(e1)
+end
+function c1159.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetTargetPlayer(1-tp)
+	Duel.SetTargetParam(500)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,500)
+end
+function c1159.damop(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Damage(p,d,REASON_EFFECT)
+end
