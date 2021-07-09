3a6a6b5a9033d6a97fddc27fb60e952888490039
Fervi <lordfervi@protonmail.com>
2021-07-09 20:26:07 +0200

    Added "Zombie Knight" card

---
+++ cardgenerator/cards/scripts/ZombieKnight.lua
@@ -0,0 +1,20 @@
+function c1160.initial_effect(c)
+	--to deck top
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(1160,0))
+	e1:SetCategory(CATEGORY_TODECK)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e1:SetCode(EVENT_TO_GRAVE)
+	e1:SetTarget(c1160.tdtg)
+	e1:SetOperation(c1160.tdop)
+	c:RegisterEffect(e1)
+end
+function c1160.tdtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,e:GetHandler(),1,0,0)
+end
+function c1160.tdop(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetHandler():IsRelateToEffect(e) then
+		Duel.SendtoDeck(e:GetHandler(),nil,0,REASON_EFFECT)
+	end
+end
