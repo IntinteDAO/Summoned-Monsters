1148da35ae9905fb73a2e20c4488d981163bc0e1
Fervi <lordfervi@protonmail.com>
2021-07-21 20:27:14 +0200

    Added "Dragon's Lance" card

---
+++ cardgenerator/cards/scripts/DragonsLance.lua
@@ -0,0 +1,31 @@
+function c1171.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_RECOVER)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e1:SetCondition(c1171.condition)
+	e1:SetTarget(c1171.target)
+	e1:SetOperation(c1171.activate)
+	c:RegisterEffect(e1)
+end
+function c1171.condition(e,tp,eg,ep,ev,re,r,rp)
+	return tp~=Duel.GetTurnPlayer()
+end
+function c1171.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local tg=Duel.GetAttacker()
+	if chkc then return chkc==tg end
+	if chk==0 then return tg:IsOnField() and tg:IsCanBeEffectTarget(e) end
+	Duel.SetTargetCard(tg)
+	local rec=tg:GetAttack()
+	Duel.SetTargetParam(rec)
+	Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,rec)
+end
+function c1171.activate(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
+		local rec=tc:GetAttack()
+		Duel.Recover(tp,rec,REASON_EFFECT)
+	end
+end
