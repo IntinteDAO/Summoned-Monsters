25d3de61ebe1a90d339ad9e811d21d6888cf4863
Fervi <lordfervi@protonmail.com>
2021-07-30 16:59:29 +0200

    Added "Confused Orc" card, Fix Queen of Destiny script

--- cardgenerator/cards/scripts/QueenOfDestiny.lua
+++ cardgenerator/cards/scripts/QueenOfDestiny.lua
@@ -1,21 +1,21 @@
-function c1178.initial_effect(c)
+function c1179.initial_effect(c)
 	--dice
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(1178,0))
+	e1:SetDescription(aux.Stringid(1179,0))
 	e1:SetCategory(CATEGORY_COIN)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1)
-	e1:SetTarget(c1178.target)
-	e1:SetOperation(c1178.operation)
+	e1:SetTarget(c1179.target)
+	e1:SetOperation(c1179.operation)
 	c:RegisterEffect(e1)
 end
-c1178.toss_coin=true
-function c1178.target(e,tp,eg,ep,ev,re,r,rp,chk)
+c1179.toss_coin=true
+function c1179.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_COIN,nil,0,tp,1)
 end
-function c1178.operation(e,tp,eg,ep,ev,re,r,rp)
+function c1179.operation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsRelateToEffect(e) and c:IsFaceup() then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_COIN)
