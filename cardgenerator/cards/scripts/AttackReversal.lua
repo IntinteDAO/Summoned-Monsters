3418dd34d8c7e825d2914cb2e3783f7172853283
Fervi <lordfervi@protonmail.com>
2021-08-06 23:52:09 +0200

    Added "Attack Reversal" card

---
+++ cardgenerator/cards/scripts/AttackReversal.lua
@@ -0,0 +1,31 @@
+function c1187.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DAMAGE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e1:SetCondition(c1187.condition)
+	e1:SetTarget(c1187.target)
+	e1:SetOperation(c1187.activate)
+	c:RegisterEffect(e1)
+end
+function c1187.condition(e,tp,eg,ep,ev,re,r,rp)
+	return tp~=Duel.GetTurnPlayer()
+end
+function c1187.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local tg=Duel.GetAttacker()
+	if chkc then return chkc==tg end
+	if chk==0 then return tg:IsOnField() and tg:IsCanBeEffectTarget(e) end
+	Duel.SetTargetCard(tg)
+	local dam=tg:GetAttack()
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,dam)
+end
+function c1187.activate(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		if Duel.NegateAttack() then
+			Duel.Damage(1-tp,tc:GetAttack(),REASON_EFFECT)
+		end
+	end
+end
