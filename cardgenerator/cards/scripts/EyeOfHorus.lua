60412c4c833b32f6b1158491be4dd8fc90f9f676
Fervi <lordfervi@protonmail.com>
2021-08-12 23:31:06 +0200

    Added "Eye of Horus" card

---
+++ cardgenerator/cards/scripts/EyeOfHorus.lua
@@ -0,0 +1,23 @@
+function c1193.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCost(c1193.cost)
+	e1:SetTarget(c1193.target)
+	e1:SetOperation(c1193.activate)
+	c:RegisterEffect(e1)
+end
+function c1193.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckLPCost(tp,1000) end
+	Duel.PayLPCost(tp,1000)
+end
+function c1193.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsFacedown,tp,0,LOCATION_ONFIELD,1,nil) end
+end
+function c1193.activate(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(Card.IsFacedown,tp,0,LOCATION_ONFIELD,nil)
+	if g:GetCount()>0 then
+		Duel.ConfirmCards(tp,g)
+	end
+end
