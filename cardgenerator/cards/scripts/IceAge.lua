c63286f2812d3920ecb6a260c2e2e1c049ea207b
Fervi <lordfervi@protonmail.com>
2021-08-13 20:31:18 +0200

    Added "Ice Age" card

---
+++ cardgenerator/cards/scripts/IceAge.lua
@@ -0,0 +1,34 @@
+function c1194.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCondition(c1194.condition)
+	e1:SetOperation(c1194.operation)
+	c:RegisterEffect(e1)
+end
+function c1194.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetCurrentPhase()==PHASE_MAIN1 and not Duel.CheckPhaseActivity()
+end
+function c1194.operation(e,tp,eg,ep,ev,re,r,rp)
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetCode(EFFECT_CANNOT_ACTIVATE)
+	e1:SetTargetRange(1,1)
+	e1:SetValue(c1194.aclimit)
+	e1:SetReset(RESET_PHASE+PHASE_END,2)
+	Duel.RegisterEffect(e1,tp)
+	--cannot set
+	local e2=Effect.CreateEffect(e:GetHandler())
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_CANNOT_SSET)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetTargetRange(1,1)
+	e2:SetTarget(aux.TRUE)
+	e2:SetReset(RESET_PHASE+PHASE_END,2)
+	Duel.RegisterEffect(e2,tp)
+end
+function c1194.aclimit(e,re,tp)
+	return re:IsActiveType(TYPE_SPELL+TYPE_TRAP)
+end
