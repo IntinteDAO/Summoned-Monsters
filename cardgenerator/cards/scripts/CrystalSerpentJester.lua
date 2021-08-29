4a689f61e831dcd757e7d21e546f75bd0ee5d360
Fervi <lordfervi@protonmail.com>
2021-08-29 23:43:50 +0200

    Added "Crystal Serpent Jester" card

---
+++ cardgenerator/cards/scripts/CrystalSerpentJester.lua
@@ -0,0 +1,14 @@
+function c1208.initial_effect(c)
+	--battle indes
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_COUNT)
+	e1:SetCountLimit(1)
+	e1:SetValue(c1208.valcon)
+	c:RegisterEffect(e1)
+end
+function c1208.valcon(e,re,r,rp)
+	return bit.band(r,REASON_BATTLE)~=0
+end
