299624da43889eb9851ad4fc05e1d74a35b887ba
Fervi <lordfervi@protonmail.com>
2021-09-04 23:43:45 +0200

    Added "Greed is Good" card

---
+++ cardgenerator/cards/scripts/GreedIsGood.lua
@@ -0,0 +1,26 @@
+function c1213.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DRAW+CATEGORY_RECOVER)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(c1213.target)
+	e1:SetOperation(c1213.activate)
+	c:RegisterEffect(e1)
+end
+function c1213.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,2) end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(2)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,2)
+	Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,1-tp,1000)
+end
+function c1213.activate(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Draw(p,d,REASON_EFFECT)
+	if Duel.Draw(p,d,REASON_EFFECT)>0 then
+		Duel.BreakEffect()
+		Duel.Recover(1-tp,1000,REASON_EFFECT)
+	end
+end
