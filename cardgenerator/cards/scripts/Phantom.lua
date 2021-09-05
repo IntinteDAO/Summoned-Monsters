fad751b2c34f59d19aee9f199e9ef4eeff534df6
Fervi <lordfervi@protonmail.com>
2021-09-05 23:51:53 +0200

    Added "Phantom" card

---
+++ cardgenerator/cards/scripts/Phantom.lua
@@ -0,0 +1,36 @@
+function c1214.initial_effect(c)
+	--flip
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(1214,0))
+	e1:SetCategory(CATEGORY_TOHAND)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_FLIP)
+	e1:SetTarget(c1214.target)
+	e1:SetOperation(c1214.operation)
+	c:RegisterEffect(e1)
+end
+function c1214.filter(c,e)
+	return c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsAbleToHand() and c:IsCanBeEffectTarget(e)
+end
+function c1214.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() and c1214.filter(chkc,e) end
+	if chk==0 then return true end
+	local g=Duel.GetMatchingGroup(c1214.filter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil,e)
+	if g:GetCount()<2 then
+		g:Clear()
+		Duel.SetTargetCard(g)
+		return
+	end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+	local sg=g:Select(tp,2,2,nil)
+	Duel.SetTargetCard(sg)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,sg,2,0,0)
+end
+function c1214.operation(e,tp,eg,ep,ev,re,r,rp)
+	local tg=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	if not tg then return end
+	local dg=tg:Filter(Card.IsRelateToEffect,nil,e)
+	if dg:GetCount()==2 then
+		Duel.SendtoHand(dg,nil,REASON_EFFECT)
+	end
+end
