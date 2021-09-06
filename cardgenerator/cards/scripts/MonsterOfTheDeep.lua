ea4754cb46d30f79fe87baece47e17c1cf4a022a
Fervi <lordfervi@protonmail.com>
2021-09-06 23:36:16 +0200

    Added "Monster of the Deep" card

---
+++ cardgenerator/cards/scripts/MonsterOfTheDeep.lua
@@ -0,0 +1,33 @@
+function c1215.initial_effect(c)
+	--search
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(1215,0))
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCost(c1215.cost)
+	e1:SetTarget(c1215.target)
+	e1:SetOperation(c1215.operation)
+	c:RegisterEffect(e1)
+	c1215.discard_effect=e1
+end
+function c1215.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsDiscardable() end
+	Duel.SendtoGrave(c,REASON_COST+REASON_DISCARD)
+end
+function c1215.filter(c)
+	return c:IsCode(1215) and c:IsAbleToHand()
+end
+function c1215.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then return Duel.IsExistingMatchingCard(c1215.filter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function c1215.operation(e,tp,eg,ep,ev,re,r,rp,chk)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,c1215.filter,tp,LOCATION_DECK,0,1,2,nil)
+	if g:GetCount()>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
