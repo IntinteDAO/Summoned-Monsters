ae4340c65fef1f8c6b0e1f2041bce8c955f5b376
Fervi <lordfervi@protonmail.com>
2021-09-21 22:26:56 +0200

    Added "Binding Souls" card

---
+++ cardgenerator/cards/scripts/BindingSouls.lua
@@ -0,0 +1,35 @@
+function c1232.initial_effect(c)
+	aux.AddCodeList(c,1072)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCondition(c1232.condition)
+	e1:SetTarget(c1232.target)
+	e1:SetOperation(c1232.activate)
+	c:RegisterEffect(e1)
+end
+function c1232.cfilter(c)
+	return c:IsFaceup() and c:IsCode(1071)
+end
+function c1232.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(c1232.cfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil)
+end
+function c1232.filter(c,e,tp)
+	return c:IsCode(1071,1072) and c:IsCanBeSpecialSummoned(e,0,tp,true,false)
+end
+function c1232.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(c1232.filter,tp,LOCATION_HAND+LOCATION_DECK,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_DECK)
+end
+function c1232.activate(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,c1232.filter,tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil,e,tp)
+	local tc=g:GetFirst()
+	if tc and Duel.SpecialSummon(tc,0,tp,tp,true,false,POS_FACEUP)~=0 then
+		tc:CompleteProcedure()
+	end
+end
