c1425c82cb6c6b959867c016e0d8bd1ca214a271
Fervi <lordfervi@protonmail.com>
2021-10-17 21:57:52 +0200

    Added "Elven Archer #2" and "Troll" cards

---
+++ cardgenerator/cards/scripts/Troll.lua
@@ -0,0 +1,8 @@
+function c1266.initial_effect(c)
+	--double attack
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_EXTRA_ATTACK)
+	e1:SetValue(1)
+	c:RegisterEffect(e1)
+end
